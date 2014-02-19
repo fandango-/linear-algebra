78567c407814f3c36cfc7273b036b848a00031aa
Abhinav Baid <abhibaid@rediffmail.com>
2014-02-19 21:33:30 +0530

    added test code

--- gso.c
+++ gso.c
@@ -1,4 +1,5 @@
 #include "flint/fmpq_mat.h"
+#include "test_helpers.c"
 
 void fmpq_mat_gso(fmpq_mat_t B, const fmpq_mat_t A)
 /* Input: A basis a1, ...., an of R^n as the columns of an m x n matrix A
@@ -29,11 +30,11 @@ void fmpq_mat_gso(fmpq_mat_t B, const fmpq_mat_t A)
 		fmpq_mat_clear(t);
 	}
 	
-	if(A->r == 0) {
+	if(!A->r) {
 		fmpq_mat_zero(B);
 		return;
 	}
-		
+			
 	for(i = 0; i < A->c; i++) {
 		for(j = 0; j < A->r; j++) {
 			fmpq_set(fmpq_mat_entry(B, j, i),
@@ -61,12 +62,15 @@ void fmpq_mat_gso(fmpq_mat_t B, const fmpq_mat_t A)
 							fmpq_mat_entry(B, k, j));
 			}
 			
-			fmpq_div(mu, num, den);
+			if(!fmpq_is_zero(den))
+			{
+				fmpq_div(mu, num, den);
 			
-			for(k = 0; k < A->r; k++) {
-				fmpq_submul(fmpq_mat_entry(B, k, i),
-							mu,
-							fmpq_mat_entry(B, k, j));
+				for(k = 0; k < A->r; k++) {
+					fmpq_submul(fmpq_mat_entry(B, k, i),
+								mu,
+								fmpq_mat_entry(B, k, j));
+				}
 			}
 		}
 	}
@@ -74,4 +78,69 @@ void fmpq_mat_gso(fmpq_mat_t B, const fmpq_mat_t A)
 	fmpq_clear(num);
 	fmpq_clear(den);
 	fmpq_clear(mu);
+}
+
+int main(void)
+{
+	int i;
+    FLINT_TEST_INIT(state);
+    
+
+    flint_printf("gso....");
+    fflush(stdout);
+
+    for (i = 0; i < 100 * flint_test_multiplier(); i++)
+    {
+        fmpq_mat_t A;
+
+        slong m, n, bits;
+
+        m = n_randint(state, 10);
+        n = n_randint(state, 10);
+
+        bits = 1 + n_randint(state, 100);
+
+        fmpq_mat_init(A, m, n);
+
+        fmpq_mat_randtest(A, state, bits);
+        
+        fmpq_mat_gso(A, A);
+        
+        fmpq_t dot;
+        fmpq_init(dot);
+        int j, k, l;
+        for(j = 0; j < n; j++) {
+			for(k = j + 1; k < n; k++) {
+				
+				if(A->r)
+				fmpq_mul(dot, 
+						 fmpq_mat_entry(A, 0, j),
+						 fmpq_mat_entry(A, 0, k));
+									  
+				for(l = 1; l < m; l++)
+				{
+					fmpq_addmul(dot,
+								fmpq_mat_entry(A, l, j),
+								fmpq_mat_entry(A, l, k));
+				}
+				
+				if (!fmpq_is_zero(dot))
+				{
+					flint_printf("FAIL:\n");
+					fmpq_print(dot);
+					flint_printf("A:\n");
+					fmpq_mat_print(A);
+					abort();
+				}	
+			}
+		}
+		
+        fmpq_mat_clear(A);
+        fmpq_clear(dot);
+    }
+
+    FLINT_TEST_CLEANUP(state);
+    
+    flint_printf("PASS\n");
+    return EXIT_SUCCESS;
 }
